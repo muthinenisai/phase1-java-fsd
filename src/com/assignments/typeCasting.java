fda7742293e548c3a75b856d12a5df27eba82d4d
muthinenisai <muthinenisai9381@gmail.com>
2023-11-27 18:19:01 +0530

    first commit

---
+++ src/com/assignments/typeCasting.java
@@ -0,0 +1,38 @@
+package com.assignments;
+
+public class typeCasting {
+
+	
+
+	public static void main(String[] args) {
+
+		System.out.println("Implicit Type casting");
+		
+		char a = 'c';
+		System.out.println("value of a:"+a);
+		
+		int b = a;
+		System.out.println("value of b:"+b);
+		
+		float c = b;
+		System.out.println("value of c:"+c);
+		
+		long d = a;
+		System.out.println("value of d:"+d);
+		
+		double e = a;
+		System.out.println("value of e:"+e);
+		
+		System.out.println();
+		
+		System.out.println("Explicit Type casting");
+		
+		double E = 99.6;
+		
+		int F = (int) E;
+		
+		System.out.println("value of E:"+E);
+		System.out.println("value of F:"+F);
+	}
+
+}
